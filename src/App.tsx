8613b784d992450b83d3f3519bcd6ab5bfd3a94f
Tomás Leggire <tomasleggire@hotmail.com>
2023-05-23 20:17:21 +0000

    Api personalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import useApi from "./hooks/useApi";
 
 function App() {
-  return <div className="App"></div>;
+  const { data } = useApi();
+
+  return (
+    <div className="App">
+      <h1>hola</h1>
+    </div>
+  );
 }
 
 export default App;
