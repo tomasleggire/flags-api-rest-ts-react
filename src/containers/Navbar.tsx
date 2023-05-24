382e5efabac9ca8fb6ff18be565b4d251d6be394
Tomás Leggire <tomasleggire@hotmail.com>
2023-05-24 19:39:14 +0000

    navbar terminado

--- src/containers/Navbar.tsx
+++ src/containers/Navbar.tsx
@@ -1,10 +1,14 @@
 import "../styles/Navbar.css";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
 
 export default function Navbar() {
   return (
     <nav className="main-nav">
       <h3 className="nav-title">Where in the world?</h3>
-      <h5 className="nav-mode">Dark Mode</h5>
+      <button className="btn-mode">
+        <BsFillMoonFill className="btn-mode-icon" />
+        <h5 className="nav-mode">Dark Mode</h5>
+      </button>
     </nav>
   );
 }
